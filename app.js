44f79835d49eabc364fecaf8af50550facf99858
Ka Wing Tsang <kawingt2@gmail.com>
2021-03-11 14:46:33 +0800

    add searchUserInput event listener

--- app.js
+++ app.js
@@ -6,6 +6,27 @@ const ui = new UI;
 const searchBtn = document.querySelector('#search-btn');
 const searchUserInput = document.querySelector('#searchUser');
 
+searchUserInput.addEventListener('keyup', (e) => {
+    if(e.keyCode === 13) {
+        ui.clearProfile();
+        if(searchUserInput.value !== '') {
+            // make http call
+            github.getUser(searchUserInput.value)
+                    .then(data => {
+                        if(data.profile.message === "Not Found") {
+                            // Show Alert
+                            ui.showAlert('User not found', 'alert alert-danger')
+                        } else {
+                            console.log(data.profile);
+                            ui.showProfile(data.profile);
+                        }
+                    })
+        }
+        // Empty search input
+        searchUserInput.value = '';
+    }
+})
+
 searchBtn.addEventListener('click', (e) => {
     ui.clearProfile();
     if(searchUserInput.value !== '') {
