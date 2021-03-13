12bede3ed400c242598f4cf34a5aece05c9490e5
Ka Wing Tsang <kawingt2@gmail.com>
2021-03-13 23:12:59 +0800

    add invalid input message

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const searchUserInput = document.querySelector('#searchUser');
 
 searchUserInput.addEventListener('keyup', (e) => {
     if(e.keyCode === 13) {
+        // Always remove invalid class
+        searchUserInput.classList.remove("is-invalid");
         // Always clear the profile and repos first to start afresh 
         ui.clearProfile();
         ui.clearRepos();
@@ -24,6 +26,9 @@ searchUserInput.addEventListener('keyup', (e) => {
                             ui.showRepo(data.repos);
                         }
                     })
+        } else {
+            // Add invalid class to show alert message in form
+            searchUserInput.classList.add("is-invalid");
         }
         // Empty search input
         searchUserInput.value = '';
@@ -37,6 +42,8 @@ searchBtn.addEventListener('click', (e) => {
     ui.clearProfile();
     ui.clearRepos();
     if(searchUserInput.value !== '') {
+        // Always remove invalid class
+        searchUserInput.classList.remove('is-invalid');
         // make http call
         github.getUser(searchUserInput.value)
                 .then(data => {
@@ -49,6 +56,9 @@ searchBtn.addEventListener('click', (e) => {
                         ui.showRepo(data.repos);
                     }
                 })
+    } else {
+        // Add invalid class to show alert message in form
+        searchUserInput.classList.add('is-invalid');
     }
     // Empty search input
     searchUserInput.value = '';
